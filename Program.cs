1ee166fe7d71c4bbdf1393b83f8631c51685458f
carloscalil <calil.carlosf@gmail.com>
2023-10-16 12:41:50 -0300

    desafio POO resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine($"Smartphone Nokia:");
+Smartphone nokia = new Nokia("789546","Modelo Nokia","111222333",32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("------------------------------------------");
+
+Console.WriteLine($"Smartphone Iphone:");
+Smartphone iphone = new Iphone("789546I","Modelo Iphone","91112223332",64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
